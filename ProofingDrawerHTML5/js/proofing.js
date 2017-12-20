c47879994c7032236c57d46519d11ccd95741385
leviport <leviport377@gmail.com>
2017-12-19 20:07:57 -0700

    First commit with new web interface, good stuff so far.

---
+++ ProofingDrawerHTML5/js/proofing.js
@@ -0,0 +1,108 @@
+/* 
+ * Custom javascript for the proofing drawer user interface
+ */
+
+// the maximum and minimum tempuratures this thing will support
+var rangeMax = 90; //fahrenheit, not celcius
+var rangeMin = 70; //fahrenheit, not celcius
+
+// the seconds counter for the timer (initialized at zero, seems sensible)
+var seconds = 0;
+// whether or not the timer is paused
+var paused = true;
+// whether or not the time interval is already set (otherwise the timer goes super fast!)
+var intervalSet = false;
+
+$(document).ready(function() {
+	//make the proofing button load the proofing page
+	$("#proofing-button").click(function() {
+		window.location.href='proofing.html';
+		return false;
+	});
+
+	//make the recipes button load the recipes page
+	$("#recipes-button").click(function() {
+		window.location.href='recipes.html';
+		return false;
+	});
+
+	//make the back button go back a page
+	$("#backarrow").click(function() {
+		window.history.back();
+	});
+
+	//make the temp plus button turn up the temp (as long as it is within the appropriate range)
+	$("#tempPlus").click(function() {
+
+		//TODO: do something about Celcius. Probably check which is set (maybe make an instance variable?)
+
+		//Extract the current set tempurature from the document (and trim off the units)
+		setTemp = $("#setTemp").html();
+		setTemp = Number(setTemp.substring(0,4));
+		if (setTemp < rangeMax) {
+			$("#setTemp").text((setTemp + 1) + ".0°F");
+		}
+	
+	});
+
+	//make the temp minus button turn down the temp (as long as it is within the appropriate range)
+	$("#tempMinus").click(function() {
+
+		//TODO: do something about Celcius. Probably check which is set (maybe make an instance variable?)
+
+		//Extract the current set tempurature from the document (and trim off the units)
+		setTemp = $("#setTemp").html();
+		setTemp = Number(setTemp.substring(0,4));
+		if (setTemp > rangeMin) {
+			$("#setTemp").text((setTemp - 1) + ".0°F");
+		}
+	
+	});
+
+	$("#lightbulb").click(function() {
+
+		//TODO: need to set timer when light is on
+		//TODO: also need to AJAX a route to tell the real life lightbulb to turn on/off
+		
+		//if the light is off, turn on the light
+		if ($("#lightbulb").attr("src") == "../img/lightbulb-gray.svg") {
+			$("#lightbulb").attr("src", "../img/lightbulb-yellow.svg");
+			paused = false;
+			startTime();
+		} else { //it's on, so turn it off
+			$("#lightbulb").attr("src", "../img/lightbulb-gray.svg");
+			paused = true;
+		}
+	});
+
+	function startTime() {
+		if (!intervalSet) {
+			setInterval(setTime, 1000);
+			intervalSet = true;
+		}
+
+		function setTime() {
+			if (!paused) {
+				++seconds;
+				$("#seconds").html(pad(seconds % 60));
+				$("#minutes").html(pad(Math.floor(seconds / 60)));
+			}
+		}
+
+		function pad(val) {
+			var valString = val + "";
+			if (valString.length < 2) {
+				return "0" + valString;
+			} else {
+				return valString;
+			}
+		}
+	}
+
+	$("#timeResetButton").click(function() {
+		seconds = 0;
+		$("#minutes").html("00");
+		$("#seconds").html("00");
+	});
+	
+}); //end of document ready function
--- bakeMain.py
+++ bakeMain.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-from ProofingDrawer_01 import *
+#from ProofingDrawer_01 import *
+# commented out for testing
 from threading import Thread
 
 app = Flask(__name__)
@@ -17,14 +18,16 @@ def index():
 @app.route('/ProofingPage/<int:val>')
 def proofing(val):
     val = bool(val)
-    ProofingDrawer.loopOn = val
+#    ProofingDrawer.loopOn = val
+# commented out for testing
     if(val == True and HTMLglobals.proofLoopOff):
         HTMLglobals.proofLoopOff = False
         t = Thread(target=startDrawer)
         t.start() 
         print("drawer START!")
     elif(val == False):
-        ProofingDrawer(False)
+#        ProofingDrawer(False)
+# commented out for testing
         HTMLglobals.proofLoopOff = True
         print("drawer STOP!")
         
@@ -32,7 +35,9 @@ def proofing(val):
 
     #here to be a thread target
 def startDrawer():
-    ProofingDrawer(True)
+    print ("yo")
+#    ProofingDrawer(True)
+# commented out for testing
 
 if __name__ == '__main__':
   app.run(debug=True, host='192.168.1.119')
